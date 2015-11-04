8378ffe14799385b139d459f860cdae2380f4f4a
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2015-11-04 17:42:31 +0200

    install.sh: add overwrite confirmation

--- install.sh
+++ install.sh
@@ -1,18 +1,37 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}"i)" && pwd)"
+
 # colors for vimdiff
 if [ ! -d ~/.vim/colors/ ]; then
+    echo "vim colors folder doesn't exist. make it..."
     mkdir -p ~/.vim/colors
 fi
-if [ ! -e ~/.vim/colors/mydiffcolors.vim ]; then
-    cp .vim/colors/mydiffcolors.vim ~/.vim/colors/
+if [ -e ~/.vim/colors/mydiffcolors.vim ]; then
+    read -p "rewrite mydiffcolor.vim? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        cp -f $DIR/.vim/colors/mydiffcolors.vim ~/.vim/colors/
+        echo " ok"
+    else
+        echo
+    fi
+else
+    echo "mydiffcolor.vim doesn't exist. make it..."
+    cp $DIR/.vim/colors/mydiffcolors.vim ~/.vim/colors/
 fi
 
 # dot-files in HOME dir
 for f in .vimrc .psqlrc .gitconfig .tmux.conf
 do
     echo "file [$f]"
-    if [ ! -e ~/$f ]; then
-        echo "link does not exists. try to create..."
-        ln -s ~/git/unix_configs/$f ~/$f
+    if [ -e ~/$f ]; then
+        read -p "already exists. rewrite? " -n 1 -r
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            ln -sf $DIR/$f ~/$f
+            echo " ok"
+        else
+            echo
+        fi
+    else
+        ln -s $DIR/$f ~/$f
     fi
 done
 

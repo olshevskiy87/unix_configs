0e5fe24026f74110735bb58924fe1988a2f09573
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2016-05-27 09:38:51 +0300

    fix install.sh by shellcheck warnings

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
+#!/usr/bin/env bash
+
 dotfiles="profile bashrc vimrc psqlrc gitconfig tmux.conf pylintrc jshintrc"
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}"i)" && pwd)"
 
 mkdir -p ~/bin
 
 function create_or_update_file {
-    if [ ! -d $1 ]; then
+    if [ ! -d "$1" ]; then
         echo "$1 folder doesn't exist. make it..."
-        mkdir -p $1
+        mkdir -p "$1"
     fi
-    if [ -e $1/$2 ]; then
+    if [ -e "$1/$2" ]; then
         read -p "rewrite $2? " -n 1 -r
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            cp -f $DIR/$3/$2 $1
+            cp -f "$DIR/$3/$2" "$1"
             echo " ok"
         else
             echo
         fi
     else
         echo "$2 doesn't exist. make it..."
-        cp $DIR/$3/$2 $1
+        cp "$DIR/$3/$2" "$1"
     fi
 }
 
@@ -33,23 +35,23 @@ create_or_update_file ~/.vim/autoload myf.vim .
 create_or_update_file ~ ssh-find-agent.sh ssh-find-agent
 
 # icdiff
-ln -sf $DIR/icdiff/icdiff ~/bin/icdiff
+ln -sf "$DIR/icdiff/icdiff" ~/bin/icdiff
 
 # dot-files in HOME dir
 for f in $dotfiles
 do
     dot_f=.$f
     echo "file [$dot_f]"
-    if [ -L ~/$dot_f ]; then
+    if [ -L "$HOME/$dot_f" ]; then
         read -p "already exists. rewrite? " -n 1 -r
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            ln -sf $DIR/$f ~/$dot_f
+            ln -sf "$DIR/$f" "$HOME/$dot_f"
             echo " ok"
         else
             echo
         fi
     else
-        ln -s $DIR/$f ~/$dot_f
+        ln -s "$DIR/$f" "$HOME/$dot_f"
     fi
 done
 
